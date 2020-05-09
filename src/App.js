5e71ef372636accb0a45d9ad62d55178b221e43d
Ayushi <aayushi.mandlik@gmail.com>
2020-05-09 09:36:40 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import {Cards, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
+import {fetchData} from './api';
+
+class App extends React.Component{
+    state = {
+        data: {},
+    }
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        //console.log(data);
+        this.setState({data: fetchedData});
+    }
+    render(){
+        //const {data} = this.state;
+        return(
+            <div className={styles.container}>
+                <Cards data={this.state.data}/>
+                <Chart/>
+                <CountryPicker/>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/api/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+    try{
+       /* const { data} = await axios.get(url);
+        const modifiedData = {
+            confirmed: data.confirmed,
+            recovered: data.recovered,
+            deaths: data.deaths,
+            lastUpdate: data.lastUpdate
+        }
+        return modifiedData;*/
+        //console.log(response);
+        const { data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);
+        return {confirmed, recovered, deaths, lastUpdate};
+    }
+    catch(error){
+
+    }
+}
+
+export const fetchDailyData = async () => {
+    try{
+        const {data} = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData)=>({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }))
+        console.log(data);
+        return modifiedData;
+    }
+    catch(error){
+
+    }
+}
